614885e6d74160dae31d341231dae61ecd39a0cd
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-05-10 16:55:14 -0300

    firts commit

--- src/test/java/com/etec/backend/HeartCareApplicationTests.java
+++ src/test/java/com/etec/backend/HeartCareApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.loginauthapi;
+package com.etec.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoginAuthApiApplicationTests {
+class HeartCareApplicationTests {
 
 	@Test
 	void contextLoads() {

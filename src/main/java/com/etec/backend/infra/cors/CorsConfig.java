23458ca27d76d9309cb76fe6168144a8cb98e0f1
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-05-10 20:11:35 -0300

    firts commit

--- src/main/java/com/etec/backend/infra/cors/CorsConfig.java
+++ src/main/java/com/etec/backend/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://heartcare-backend.onrender.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
--- src/main/java/com/etec/backend/infra/security/SecurityConfig.java
+++ src/main/java/com/etec/backend/infra/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/profile/create").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "api/v1/profile/update/{id}").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/v1/profile/get").permitAll()
 
                         .anyRequest().authenticated())

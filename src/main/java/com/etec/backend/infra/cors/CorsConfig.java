f29cd2615c4caf088aff9740d8d190b6b2d50b71
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-06-30 17:24:53 -0300

    firts commit

--- src/main/java/com/etec/backend/infra/cors/CorsConfig.java
+++ src/main/java/com/etec/backend/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:4200")
+        .allowedOrigins("http://localhost:5173")
         .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }

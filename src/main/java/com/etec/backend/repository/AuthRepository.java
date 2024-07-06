257405b94a9df4d6d3d4ba4678addb9a9a00c693
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-07-06 09:41:38 -0300

    firts commit

--- src/main/java/com/etec/backend/repository/AuthRepository.java
+++ src/main/java/com/etec/backend/repository/AuthRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.etec.backend.entity.Auth;
 
-
 public interface AuthRepository extends JpaRepository<Auth, Long> {
     Optional<Auth> findByEmail(String email);
 }

4e7d9579bf6334d7d5729e9beff47a6ba5e3a27c
sntooos <Juliano.santos88@icloud.com>
2024-09-04 19:33:04 -0300

    firts commit

--- src/main/java/com/etec/backend/repository/PressureRepository.java
+++ src/main/java/com/etec/backend/repository/PressureRepository.java
@@ -8,13 +8,32 @@ import com.etec.backend.dto.PressureConcatResponseDTO;
 import com.etec.backend.entity.Pressure;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface PressureRepository extends JpaRepository<Pressure, Long> {
 
-    List<Pressure> findByUserId(Long userId);
+    @Query(value = "SELECT * FROM tb02_pressure WHERE tb02_user_id = :userId", nativeQuery = true)
+    List<Pressure> findByUserId(@Param("userId") Long userId);
 
-    @Query("SELECT new com.etec.backend.dto.PressureConcatResponseDTO(p.id, p.diastolic, p.systolic, p.pulse, p.date, u.id, u.name, u.lastname, u.photo) " +
-            "FROM Pressure p JOIN p.user u")
-    List<PressureConcatResponseDTO> listarTodosComDadosUserDTO();
+    @Query(value = "SELECT " +
+            "tb02_pressure.tb02_id, " +
+            "tb02_pressure.tb02_diastolic, " +
+            "tb02_pressure.tb02_systolic, " +
+            "tb02_pressure.tb02_pulse, " +
+            "tb02_pressure.tb02_date, " +
+            "tb00_users.tb00_id, " +
+            "tb00_users.tb00_name, " +
+            "tb00_users.tb00_lastname, " +
+            "tb00_users.tb00_photo " +
+            "FROM tb02_pressure " +
+            "JOIN tb00_users ON tb02_pressure.tb02_user_id = tb00_users.tb00_id", nativeQuery = true)
+    List<Object[]> listarTodosComDadosUser();
+
+    default List<PressureConcatResponseDTO> listarTodosComDadosUserDTO() {
+        List<Object[]> results = listarTodosComDadosUser();
+        return results.stream()
+                .map(PressureConcatResponseDTO::new)
+                .collect(Collectors.toList());
+    }
 
 }

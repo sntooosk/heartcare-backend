f036e9b8a7cd438366078f6ca9763dbab0d49f21
sntooos <Juliano.santos88@icloud.com>
2024-09-05 08:48:10 -0300

    update

--- src/main/java/com/etec/backend/repositories/PressureRepository.java
+++ src/main/java/com/etec/backend/repositories/PressureRepository.java
@@ -2,7 +2,6 @@ package com.etec.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.etec.backend.entities.Pressure;
 import com.etec.backend.entities.dtos.PressureConcatResponseDTO;

9f37fe1702bd828dcbf6b1b7861ae474e6b48928
sntooos <Juliano.santos88@icloud.com>
2024-10-28 17:35:08 -0300

    delete: Chat doctor e user

--- src/main/java/com/etec/backend/dtos/ChatRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.etec.backend.dtos;
-
-public record ChatRequestDTO(Long senderId, Long recipientId, String content) {
-}
--- src/main/java/com/etec/backend/dtos/ChatResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package com.etec.backend.dtos;
-
-import java.util.Date;
-
-public record ChatResponseDTO(Long senderId, Long recipientId, String content, Date dateSender) {
-}
